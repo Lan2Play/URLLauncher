3a803977b12ab7efd49c11c5e8962a111191804b
Apfelwurm <Alexander@volzit.de>
2020-10-10 14:28:07 +0200

    added removing of the last / option

--- Program.cs
+++ Program.cs
@@ -8,12 +8,19 @@ namespace urllauncher
     {
         static void Main(string[] args)
         {
-            if (args.Length == 3 || (args.Length == 1 && args[0] == "/?"))
+            if (args.Length == 3 || args.Length == 4 || (args.Length == 1 && args[0] == "/?"))
             {
-                if (args.Length == 3)
+                if (args.Length == 3 || args.Length == 4)
                 {
                     string urlparam = Regex.Replace(args[0], "^.*://", "");
-
+                    
+                    if ( args.Length == 4 && args[3] == "true")
+                    {
+                        if (urlparam.Substring(urlparam.Length - 1) == "/")
+                        {
+                            urlparam = urlparam.Remove(urlparam.Length - 1);
+                        }
+                    }
 
                     var process = new Process();
                     process.StartInfo.FileName = args[1];
@@ -21,6 +28,7 @@ namespace urllauncher
                     process.StartInfo.UseShellExecute = false;
                     process.StartInfo.CreateNoWindow = true;
                     process.Start();
+                    
                 }
                 if (args[0] == "/?")
                 {
@@ -28,7 +36,7 @@ namespace urllauncher
                     Console.WriteLine();
                     Console.WriteLine("This software removes an URL Prefix from the passed url and adds it to the end of the arguments parameter which then gets passed to the destination software. It becomes handy to use custom URL Handlers on windows with software that dont support handling full urls in Parameters.");
                     Console.WriteLine();
-                    Console.WriteLine("example: urllauncher.exe <url> <software to startup> <software to startup args>");
+                    Console.WriteLine("example: urllauncher.exe <url> <software to startup> <software to startup args> <optional: true for removing the end / of the url> ");
                 }
 
 
