b1ba2d544dc1dbd30e560d8bab62964e7dd6a68d
Apfelwurm <Alexander@volzit.de>
2020-10-10 13:51:54 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace urllauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3 || (args.Length == 1 && args[0] == "/?"))
+            {
+                if (args.Length == 3)
+                {
+                    string urlparam = Regex.Replace(args[0], "^.*://", "");
+
+
+                    var process = new Process();
+                    process.StartInfo.FileName = args[1];
+                    process.StartInfo.Arguments = args[2] + urlparam;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+                }
+                if (args[0] == "/?")
+                {
+                    Console.WriteLine("URLLauncher");
+                    Console.WriteLine();
+                    Console.WriteLine("This software removes an URL Prefix from the passed url and adds it to the end of the arguments parameter which then gets passed to the destination software. It becomes handy to use custom URL Handlers on windows with software that dont support handling full urls in Parameters.");
+                    Console.WriteLine();
+                    Console.WriteLine("example: urllauncher.exe <url> <software to startup> <software to startup args>");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("You must pass 3 Arguments! open with /? to get help!");
+            }
+        }
+    }
+}
